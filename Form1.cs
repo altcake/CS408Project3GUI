842bfb4fa2b58dd0012b00331b82095b2600806f
Ara Malayan <aramalayan@gmail.com>
2014-12-03 11:03:51 -0800

    Zero added for empty string
    Zero is now displayed when cleared

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,7 @@ namespace CS408Project3GUI
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = "0";
         }
         private void textBox1_KeyPress(object sender, KeyEventArgs e)
         {
@@ -83,12 +84,26 @@ namespace CS408Project3GUI
 
         private void button5_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 5;
+            if(textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += 5;
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 7;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += 7;
+            }
         }
 
         private void EqualsButton_Click(object sender, EventArgs e)
@@ -138,13 +153,13 @@ namespace CS408Project3GUI
                 try
                 {
                     value = Double.Parse(textBox1.Text);
+                    textBox1.Text += "/";
                 }
                 catch (System.FormatException ex)
                 {
                     MessageBox.Show("Input was in an unexpected format.", "You messed up");
                     textBox1.ClearUndo();
                 }
-                textBox1.Text += "/";
                 op = true;
                 DecimalUsed = false;
         }
@@ -157,13 +172,13 @@ namespace CS408Project3GUI
                 try
                 {
                     value = Double.Parse(textBox1.Text);
+                    textBox1.Text += "*";
                 }
                 catch (System.FormatException ex)
                 {
                     MessageBox.Show("Input was in an unexpected format.", "You messed up");
                     textBox1.ClearUndo();
                 }
-                textBox1.Text += "*";
                 op = true;
                 DecimalUsed = false;
         }
@@ -176,13 +191,13 @@ namespace CS408Project3GUI
                 try
                 {
                     value = Double.Parse(textBox1.Text);
+                    textBox1.Text += "+";
                 }
                 catch (System.FormatException ex)
                 {
                     MessageBox.Show("Input was in an unexpected format.", "You messed up");
                     textBox1.ClearUndo();
                 }
-                textBox1.Text += "+";
                 op = true;
                 DecimalUsed = false;
         }       
@@ -196,13 +211,13 @@ namespace CS408Project3GUI
                 try
                 {
                     value = Double.Parse(textBox1.Text);
+                    textBox1.Text += "-";
                 }
                 catch (System.FormatException ex)
                 {
                     MessageBox.Show("Input was in an unexpected format.", "You messed up");
                     textBox1.ClearUndo();
                 }
-                textBox1.Text += "-";
                 op = true;
                 DecimalUsed = false;
         }
@@ -229,47 +244,103 @@ namespace CS408Project3GUI
 
         private void button0_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 0;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += 0;
+            }
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 9;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += 9;
+            }
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 8;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += 8;
+            }
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
-            textBox1.Clear();
+            textBox1.Text = "0";
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 6;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += 6;
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 4;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += 4;
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 3;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += 3;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 2;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += 2;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text += 1;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += 1;
+            }
         }
     }
 }
